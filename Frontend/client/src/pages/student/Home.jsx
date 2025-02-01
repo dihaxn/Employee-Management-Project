7ccc3ac83e7cc99df56c0cc1c37804ab094fc453
dihaxn <ranasinghe12377@usci.ruh.ac.lk>
2025-02-01 22:28:37 +0530

    hero component bug fix

--- Frontend/client/src/pages/student/Home.jsx
+++ Frontend/client/src/pages/student/Home.jsx
@@ -1,10 +1,9 @@
-// Home.jsx
 import React from 'react';
 import Hero from "../../component/student/Hero.jsx";
 
 const Home = () => {
     return (
-        <div className='flex flex-col items-center space-y-7'>
+        <div className='flex flex-col items-center space-y-7 text-[15px] leading-[21px]'>
             <Hero/>
         </div>
     );
