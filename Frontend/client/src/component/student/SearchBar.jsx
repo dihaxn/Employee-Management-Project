52a074a8344f6945d81ebabf3f91d8d13f92a30b
dihaxn <ranasinghe12377@usci.ruh.ac.lk>
2025-02-03 15:22:05 +0530

    make search bar completely

--- Frontend/client/src/component/student/SearchBar.jsx
+++ Frontend/client/src/component/student/SearchBar.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { assets } from '../../assets/assets.js';
+import {data, useNavigate} from "react-router-dom";
 
-const SearchBar = () => {
-    return(
-        <div>
+const SearchBar = ({data}) => {
 
-        </div>
+    const navigate = useNavigate();
+    const [input,setInput] = useState(data ? data : '' );
 
-    )
-}
+    const onSearchHandler = (e) => {
+        e.preventDefault();
+        navigate('/course-list' + input);
+    }
 
-export default  SearchBar;
+    return (
+        <form onSubmit={onSearchHandler} className='max-w-xl w-full h-14 flex items-center bg-white border border-gray-500/20 rounded'>
+            <img src={assets.search_icon} alt="Search icon" className='w-10 px-3' />
+            <input onChange={e=>setInput(e.target.value)} value={input}
+                   type="text"
+                   placeholder='Search for courses'
+                   className='w-full h-full outline-none text-gray-500/80'
+            />
+            <button
+                type='submit'
+                className='bg-blue-600 rounded text-white px-7 py-2 mx-1'
+            >
+                Search
+            </button>
+        </form>
+    );
+};
+
+export default SearchBar;
--- Frontend/client/src/pages/student/Home.jsx
+++ Frontend/client/src/pages/student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from "../../component/student/Hero.jsx";
+import Companies from "../../component/student/Companies.jsx";
 
 const Home = () => {
     return (
-        <div className='flex flex-col items-center space-y-7 text-[15px] leading-[21px]'>
+        <div className='flex flex-col items-center space-y-7 text-[15px] leading-[21px] text-center'>
             <Hero/>
+            <Companies/>
         </div>
     );
 };
