db68b5e9096153ee40e80a982b0cb4c116b5f9ed
dihaxn <ranasinghe12377@usci.ruh.ac.lk>
2025-02-04 13:19:44 +0530

    merge bug fix

--- Frontend/client/src/pages/student/Home.jsx
+++ Frontend/client/src/pages/student/Home.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import Hero from "../../component/student/Hero.jsx";
-<<<<<<< HEAD
-
-const Home = () => {
-    return (
-        <div className='flex flex-col items-center space-y-7 text-[15px] leading-[21px]'>
-            <Hero/>
-=======
 import Companies from "../../component/student/Companies.jsx";
 import CoursesSection from "../../component/student/CoursesSection.jsx";
 
@@ -16,7 +9,6 @@ const Home = () => {
             <Hero/>
             <Companies/>
             <CoursesSection/>
->>>>>>> dd6d3145fb306b62c7cf04a3afd89c4a1ab5e66d
         </div>
     );
 };
