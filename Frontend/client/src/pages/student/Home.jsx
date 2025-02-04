ebee9c3333373f7b2611b24707d2c2848216c090
dihaxn <ranasinghe12377@usci.ruh.ac.lk>
2025-02-04 12:49:18 +0530

    Done upto course cart

--- Frontend/client/src/pages/student/Home.jsx
+++ Frontend/client/src/pages/student/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from "../../component/student/Hero.jsx";
 import Companies from "../../component/student/Companies.jsx";
+import CoursesSection from "../../component/student/CoursesSection.jsx";
 
 const Home = () => {
     return (
         <div className='flex flex-col items-center space-y-7 text-[15px] leading-[21px] text-center'>
             <Hero/>
             <Companies/>
+            <CoursesSection/>
         </div>
     );
 };
